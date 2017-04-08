c2444ed6abea0d61a7373a9131d55a3fd57dce13
Maksym Pylypenko <maksym.pylypenko@omnigon.com>
2017-04-08 21:54:50 +0300

    Homework: script that will count workdays and holidays

---
+++ workdays.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import calendar
+import datetime
+my_calendar = calendar.Calendar()
+months = [1,2,3,4,5,6,7,8,9,10,11,12]
+years = [2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020]
+
+def show_month(current_year, current_month):
+    dates = my_calendar.monthdatescalendar(current_year, current_month)
+    # Initialize the counter
+    holidays_count = 0
+    workdays_count = 0
+    # Define static holidays
+    new_year = datetime.date(current_year, 1, 1)
+    feb_23 = datetime.date(current_year, 2, 23)
+    march_8 = datetime.date(current_year, 3, 8)
+    
+    for date in dates:
+        for day in date:
+            
+            if (day.year == current_year) and (day.month == current_month):
+                # Calendar module operates on weeks, so a week may contain previous
+                # or next month's days.
+                # That's why I have to filter those out.
+                if (day == new_year or day == feb_23 or day == march_8) and (day.isoweekday() == 6 or day.isoweekday() == 7):
+                    # static holiday is on weekend
+                    #print str(day) + "        празднег в выходно " + str(day.isoweekday())
+                    holidays_count += 1
+                elif (day == new_year or day == feb_23 or day == march_8) and (day.isoweekday() != 6 or day.isoweekday() != 7):
+                    # static holiday on a regular day
+                    #print str(day) + "        празднег " + str(day.isoweekday())
+                    holidays_count += 1
+                elif (day.isoweekday() == 6 or day.isoweekday() == 7):
+                    # just a Saturday or Sunday
+                    #print str(day)+ " выходно"
+                    holidays_count += 1
+                else:
+                    # Workday
+                    workdays_count += 1
+    return workdays_count, holidays_count
+
+for year in years:
+    total_workdays = 0
+    total_holidays = 0
+    for month in months:
+        this_month_workdays, this_month_holidays = show_month(year,month)
+        total_workdays += this_month_workdays
+        total_holidays += this_month_holidays
+    print str(year) + " год - " + str(total_workdays) + " рабочих дней, " + str(total_holidays) + " выходных"
